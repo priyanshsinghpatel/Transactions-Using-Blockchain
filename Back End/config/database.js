5bdc82d762ac5634c87ccc1611804da8df5bc5d4
priyanshsinghpatel <patelpsp206@gmail.com>
2023-03-17 17:42:52 +0530

    Back End

---
+++ Back End/config/database.js
@@ -0,0 +1,17 @@
+module.exports = ({ env }) => ({
+  connection: {
+    client: 'postgres',
+    connection: {
+      host: env('DATABASE_HOST', 'localhost'),    
+      port: env.int('DATABASE_PORT', 5432),
+      database: env('DATABASE_NAME', 'Transactions-Using-Blockchain'),
+      user: env('DATABASE_USERNAME', 'postgres'),
+      password: env('DATABASE_PASSWORD', 'ashu'),
+      schema: env('DATABASE_SCHEMA', 'public'), // Not required
+      //  ssl: {
+      //    rejectUnauthorized: false,
+      //  },
+    },
+    // debug: false,
+  },
+});
