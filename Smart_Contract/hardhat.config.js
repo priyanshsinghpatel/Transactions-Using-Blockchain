389eb2a6253d0b53a5c129e66ff35ce4b2caf7b5
priyanshsinghpatel <patelpsp206@gmail.com>
2023-04-07 13:07:06 +0530

    Sevices section added

--- Smart_Contract/hardhat.config.js
+++ Smart_Contract/hardhat.config.js
@@ -1,6 +1,11 @@
-require("@nomicfoundation/hardhat-toolbox");
+require('@nomiclabs/hardhat-waffle');
 
-/** @type import('hardhat/config').HardhatUserConfig */
-module.exports = {
-  solidity: "0.8.18",
+module.exports={
+  solidity:'0.8.0',
+  networks: {
+    goerli:{
+      url:"https://eth-goerli.g.alchemy.com/v2/UdtKz0Zi2DgQxN6cnobW958LRGUUEErV",
+      accounts:['0a5d0c383c318a779752fe859da0a6ef2510d2a67ef2025856b138300494fc5f']
+    },
+  },
 };
