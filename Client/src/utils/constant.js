c86a9a63daa83389fd9b9c384cf01b1de1eacb8f
priyanshsinghpatel <patelpsp206@gmail.com>
2023-04-17 10:40:11 +0530

    Transaction Context  page is added

---
+++ Client/src/utils/constant.js
@@ -0,0 +1,4 @@
+import abi from './Transactions.json';
+
+export const contractABI= abi.abi;
+export const contractAddress= "0x9e3D81beF95345bE9b09f03B051319e2a151BCef";
--- Smart_Contract/hardhat.config.js
+++ Smart_Contract/hardhat.config.js
@@ -3,8 +3,8 @@ require('@nomiclabs/hardhat-waffle');
 module.exports={
   solidity:'0.8.0',
   networks: {
-    goerli:{
-      url:"https://eth-goerli.g.alchemy.com/v2/UdtKz0Zi2DgQxN6cnobW958LRGUUEErV",
+    sepolia:{
+      url:"https://eth-sepolia.g.alchemy.com/v2/hBj9GaxUhzWurLDLh_vKV1wM1BuQQBFD",
       accounts:['0a5d0c383c318a779752fe859da0a6ef2510d2a67ef2025856b138300494fc5f']
     },
   },
